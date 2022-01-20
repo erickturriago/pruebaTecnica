28d3798e0dd48f2fcf52e8ad81988ab9baa95094
Erick <turriago-erick@hotmail.com>
2022-01-20 08:23:24 -0500

    Se agrega tabla donde se mostrarán los datos

--- index.php
+++ index.php
@@ -5,8 +5,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de terceros</title>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
 <body>
+    <div class="container">
+        <br>
+        <h1 class="text-center">Listado de terceros</h1>
+        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#insertar">Agregar</button>
+        
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col" class="text-center">Departamento</th>
+            <th scope="col" class="text-center">Municipio</th>
+            <th scope="col" class="text-center">Identificación</th>
+            <th scope="col" class="text-center">Nombres</th>
+            <th scope="col" class="text-center">Tipo Tecero</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
     
 </body>
 </html>
